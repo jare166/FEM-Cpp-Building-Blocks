ecec2d4653ce2c8e177f2ba2115bbc5f178b0d44
Dominic Jarecki <jare166@tamu.edu>
2023-05-28 15:35:55 -0500

    Hopefully these are the last-required pass by references.

--- C_Matrix_Dense.h
+++ C_Matrix_Dense.h
@@ -466,7 +466,7 @@ class C_Matrix_Dense {
     }
 
     //! Matrix Multiplication
-    C_Matrix_Dense operator*(C_Matrix_Dense& obj2) {
+    C_Matrix_Dense& operator*(C_Matrix_Dense& obj2) {
         /*!
         Overloads * operator to allow two matrices to be multiplied.
 
@@ -497,7 +497,7 @@ class C_Matrix_Dense {
         return obj_out;
     }   
     //! Matrix Addition
-    C_Matrix_Dense operator+(C_Matrix_Dense& obj2) {
+    C_Matrix_Dense& operator+(C_Matrix_Dense& obj2) {
         // Check: Ensure both matrices are the same size
         check_size_addition((*this), obj2, "Addition");
 
@@ -508,7 +508,7 @@ class C_Matrix_Dense {
         return obj_out;
     }
     //! Matrix Subtraction
-    C_Matrix_Dense operator-(C_Matrix_Dense& obj2) {
+    C_Matrix_Dense& operator-(C_Matrix_Dense& obj2) {
         // Check: Ensure both matrices are the same size
         check_size_addition((*this), obj2, "Subtraction");
 
